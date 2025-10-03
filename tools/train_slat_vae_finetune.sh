6fed1e208488e6b0e22e3aaa07e4455fe2c40d9c
Yue Gao <yuegao@cs.stanford.edu>
2025-10-03 14:02:17 -0700

    update

--- tools/train_slat_vae_finetune.sh
+++ tools/train_slat_vae_finetune.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=1
 
 python train.py \
-    --config configs/vae/slat_vae_enc_dec_gs_swin8_B_64l8_fp16_finetune.json \
+    --config configs/vae_finetune/slat_vae_enc_dec_gs_swin8_B_64l8_fp16_finetune.json \
     --output_dir /viscam/projects/4d-state-machine/TRELLIS_outputs/slat_vae_enc_dec_gs_flora4d \
     --data_dir /scr/yuegao/TRELLIS_datasets/Flora4D_train \
     --master_port 69901 \
--- tools/train_ss_vae_finetune.sh
+++ tools/train_ss_vae_finetune.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python train.py \
-    --config configs/vae/ss_vae_conv3d_16l8_fp16_finetune.json \
+    --config configs/vae_finetune/ss_vae_conv3d_16l8_fp16_finetune.json \
     --output_dir /viscam/projects/4d-state-machine/TRELLIS_outputs/ss_vae_flora4d \
     --data_dir /scr/yuegao/TRELLIS_datasets/Flora4D_train \
     --master_port 69900 \
